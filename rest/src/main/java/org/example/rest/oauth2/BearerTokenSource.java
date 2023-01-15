bdd86e7db5c2c99598b1ee9dedc4666860aca80d
Cameron Prater <40479627+cottoncammy@users.noreply.github.com>
2023-01-15 00:01:44 -0500

    bugfixes

--- rest/src/main/java/org/example/rest/oauth2/BearerTokenSource.java
+++ rest/src/main/java/org/example/rest/oauth2/BearerTokenSource.java
@@ -3,6 +3,7 @@ package org.example.rest.oauth2;
 import static java.util.Objects.requireNonNull;
 
 import io.smallrye.mutiny.Uni;
+import io.vertx.ext.auth.JWTOptions;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
 import io.vertx.ext.auth.oauth2.OAuth2Options;
 import io.vertx.ext.auth.oauth2.Oauth2Credentials;
@@ -11,20 +12,27 @@ import io.vertx.mutiny.ext.auth.User;
 import io.vertx.mutiny.ext.auth.oauth2.OAuth2Auth;
 import org.example.rest.request.AccessTokenSource;
 import org.example.rest.resources.oauth2.AccessToken;
+import org.example.rest.resources.oauth2.Scope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BearerTokenSource implements AccessTokenSource {
     private static final Logger LOG = LoggerFactory.getLogger(BearerTokenSource.class);
     private final Vertx vertx;
     private final OAuth2Auth oAuth2;
     private final OAuth2FlowType flowType;
+
     @Nullable
     private final String code;
     @Nullable
     private final String redirectUrl;
+    @Nullable
+    private final EnumSet<Scope> scopes;
 
     private volatile User user;
 
@@ -35,16 +43,24 @@ public class BearerTokenSource implements AccessTokenSource {
                 .setSite("https://discord.com/api/oauth2")
                 .setAuthorizationPath("/authorize")
                 .setTokenPath("/token")
-                .setRevocationPath("/token/revoke");
+                .setRevocationPath("/token/revoke")
+                .setUserAgent(String.format("DiscordBot (%s, %s)", "https://github.com/cameronprater/discord-TODO", "0.1.0"));
         return new Builder(requireNonNull(vertx, "vertx"), OAuth2Auth.create(vertx, options));
     }
 
-    private BearerTokenSource(Vertx vertx, OAuth2Auth oAuth2, OAuth2FlowType flowType, String code, String redirectUrl) {
+    private BearerTokenSource(
+            Vertx vertx,
+            OAuth2Auth oAuth2,
+            OAuth2FlowType flowType,
+            String code,
+            String redirectUrl,
+            EnumSet<Scope> scopes) {
         this.vertx = vertx;
         this.oAuth2 = oAuth2;
         this.flowType = flowType;
         this.code = code;
         this.redirectUrl = redirectUrl;
+        this.scopes = scopes;
     }
 
     @Override
@@ -52,13 +68,15 @@ public class BearerTokenSource implements AccessTokenSource {
         Uni<User> uni = Uni.createFrom().item(user);
         if (user != null) {
             if (user.expired()) {
-                LOG.debug("Refreshing expired access token for user {}", user.subject());
+                LOG.debug("Refreshing expired access token");
                 uni = oAuth2.refresh(user).invoke(user -> this.user = user);
             }
         } else {
             Oauth2Credentials credentials = new Oauth2Credentials().setFlow(flowType);
             if (flowType == OAuth2FlowType.AUTH_CODE) {
                 credentials.setCode(code).setRedirectUri(redirectUrl);
+            } else {
+                credentials.setScopes(scopes.stream().map(Scope::getValue).collect(Collectors.toList()));
             }
             uni = oAuth2.authenticate(credentials).invoke(user -> this.user = user);
         }
@@ -85,11 +103,12 @@ public class BearerTokenSource implements AccessTokenSource {
                     oAuth2,
                     OAuth2FlowType.AUTH_CODE,
                     requireNonNull(code, "code"),
-                    requireNonNull(redirectUrl, "redirectUrl"));
+                    requireNonNull(redirectUrl, "redirectUrl"),
+                    null);
         }
 
-        public BearerTokenSource fromClientCredentials() {
-            return new BearerTokenSource(vertx, oAuth2, OAuth2FlowType.CLIENT, null, null);
+        public BearerTokenSource fromClientCredentials(EnumSet<Scope> scopes) {
+            return new BearerTokenSource(vertx, oAuth2, OAuth2FlowType.CLIENT, null, null, requireNonNull(scopes, "scopes"));
         }
     }
 }
